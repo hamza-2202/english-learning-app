eacb6f2d70e29649f5c7b501a8721ee90a3fac6a
hamza-2202 <hs22082002@gmail.com>
2025-11-21 22:19:12 +0500

    Fixed CORS problem

--- middlewares/error.middleware.mjs
+++ middlewares/error.middleware.mjs
@@ -1,6 +1,10 @@
-export const errorHandler =(err, request, response, next)=> {
+export const errorHandler = (err, request, response, next) => {
+
+    response.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    response.header("Access-Control-Allow-Credentials", "true");
+
     const statusCode = response.statusCode || 500
-    
+
     response.status(statusCode).json({
         message: err.message,
         stack: err.stack
--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express()
 const port = process.env.PORT
 connectDB()
 
-app.use(cors({ origin: '*' }))
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}))
 
 app.use(express.json())
 app.use(passport.initialize())
